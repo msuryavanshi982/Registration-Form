1072e70928508a7eabad997ce6d44f698c4a7ce4
Meenakshi Lodhi Rajput <rajputmeenakshi982@gmail.com>
2023-02-11 00:14:36 +0530

    calculator

---
+++ script.js
@@ -0,0 +1,39 @@
+function display(val) {
+  document.getElementById("textval").value += val;
+}
+
+function evaluateme() {
+  let x = document.getElementById("textval").value;
+  let y = eval(x);
+  let operations = ["+", "-", "*", "/"];
+  let result = "";
+
+  for (let i = 0; i < operations.length; i++) {
+    let expressions = x.split(operations[i]);
+    let isOperatorPresent = expressions.length == 2;
+    if (isOperatorPresent) {
+      let operand1 = expressions[0];
+      let operand2 = expressions[1];
+      switch (operations[i]) {
+        case "+":
+          result = (parseFloat(operand1) + parseFloat(operand2)).toFixed(2);
+          break;
+        case "-":
+          result = operand1 - operand2;
+          break;
+        case "*":
+          result = operand1 * operand2;
+          break;
+        case "/":
+          result = operand1 / operand2;
+          break;
+      }
+      break;
+    }
+  }
+  document.getElementById("textval").value = result;
+}
+
+function clr() {
+  document.getElementById("textval").value = "";
+}
